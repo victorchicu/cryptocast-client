77167a9f9b67c3ea87a0c9787ab5fbeb54603f9b
Victor Chicu <victor@aroundwire.com>
2022-01-29 22:36:32 +0200

    Added chips service implementation.

---
+++ src/app/services/chips/chip-service.ts
@@ -0,0 +1,39 @@
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {BaseService} from "../base-service";
+import {Observable} from "rxjs";
+import {catchError} from "rxjs/operators";
+import {ChipDto} from "../../shared/dto/chip-dto";
+
+export class ChipService extends BaseService {
+  static readonly API_PATH: string = "/api/chips"
+
+  constructor(protected httpClient: HttpClient) {
+    super(ChipService.API_PATH, httpClient);
+  }
+
+  public addChip(chipDto: ChipDto): Observable<ChipDto> {
+    return this.httpClient.post<ChipDto>(ChipService.API_PATH, chipDto, this.httpOptions)
+      .pipe(
+        catchError(this.handleError<ChipDto>('addChip'))
+      )
+  }
+
+
+  public removeChip(name: string, chipDto: ChipDto): Observable<void> {
+    const url: string = `${ChipService.API_PATH}/${name}/remove`;
+    return this.httpClient.delete<void>(url, this.httpOptions)
+      .pipe(
+        catchError(this.handleError<void>('removeChip'))
+      )
+  }
+
+  public listChips(params: HttpParams): Observable<ChipDto[]> {
+    const options = {
+      params: params,
+    }
+    return this.httpClient.get<ChipDto[]>(ChipService.API_PATH, options)
+      .pipe(
+        catchError(this.handleError<ChipDto[]>('listChips'))
+      )
+  }
+}
---
+++ src/app/shared/dto/chip-dto.ts
@@ -0,0 +1,9 @@
+import {ExchangeProvider} from "../enums/exchange-provider";
+
+export class ChipDto {
+  private name: string
+
+  constructor(name: string) {
+    this.name = name;
+  }
+}
